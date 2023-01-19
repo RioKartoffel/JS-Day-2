bb06b0b8b2ea8c207c9b6f15afd34e3bc23e0689
Rio-Kartoffel <riokartoffel09@gmail.com>
2023-01-19 20:31:46 +0800

    edit

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ next.addEventListener('click', () => {
     update()
 })
 
-prev.addEventListener('click', => {
+prev.addEventListener('click', () => {
     currentActive--
 
     if(currentActive < 1) {
@@ -26,22 +26,22 @@ prev.addEventListener('click', => {
 })
 
 function update() {
-    circles.forEach((circle,idx) => {
-        if(idx <currentActive) {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
             circle.classList.add('active')
         } else {
             circle.classList.remove('active')
         }
     })
 
-    const actives =document.querySelectorAll('active')
+    const actives =document.querySelectorAll('.active')
 
     progress.style.width = (actives.length - 1) / (circles.length -1) * 100 + '%'
 
     if(currentActive === 1) {
         prev.disabled = true
     } else if (currentActive === circles.length) {
-        next.disabled =true
+        next.disabled = true
     } else {
         prev.disabled = false
         next.disabled = false
